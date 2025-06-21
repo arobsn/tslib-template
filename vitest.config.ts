81710f30b672f53e9100c09095cf9970a25b190f
arobsn <87387688+arobsn@users.noreply.github.com>
2025-06-21 18:19:07 -0300

    use `v8` coverage provider

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,12 +3,13 @@ import { coverageConfigDefaults, defineConfig } from "vitest/config";
 // Reference: https://vitest.dev/config/
 export default defineConfig({
   test: {
-    // setup 'github-actions' reporter if running in GitHub Actions
+    // use 'github-actions' reporter if running in GitHub Actions
     reporters: process.env.GITHUB_ACTIONS ? ["default", "github-actions"] : ["default"],
     coverage: {
       reporter: ["text", "lcov"],
-      provider: "istanbul", // we choose 'istanbul' because it supports both Bun and Node.js
+      provider: "v8",
       clean: true,
+      include: ["src/**/*.ts"], // Include all TypeScript files in the src directory
       // Extend the default exclude patterns to exclude test files
       exclude: [...coverageConfigDefaults.exclude, "**/*.spec.ts", "**/*.test.ts"]
     }
